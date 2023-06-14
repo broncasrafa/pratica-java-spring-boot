866752e4b783acbdab63f24a76702c161c78b1dc
Rafael Francisco <broncasrafa@gmail.com>
2023-06-14 08:37:38 -0300

    adding h2 in memory database for tests

--- src/main/java/com/rsfrancisco/course/entities/User.java
+++ src/main/java/com/rsfrancisco/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.rsfrancisco.course.entities;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1;
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
